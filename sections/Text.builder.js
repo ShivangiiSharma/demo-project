bdb1dc449b6bfcbab61841d61e42f9df6911925f
Shivangi Sharma <shivangi1262.cse19@chitkara.edu.in>
2023-04-03 11:55:12 +0530

    modified file

---
+++ sections/Text.builder.js
@@ -0,0 +1,22 @@
+import dyamic from 'next/dynamic'
+import { Builder } from '@builder.io/react'
+
+const Text = dynamic(async () => {
+    return (aawait import('./Text')).default
+})
+
+Builder.registerComponent(Text, {
+    name : 'Text Element',
+    inputs: [
+        {
+        name: 'headline',
+        type: 'string',
+        defaultValue: 'This is a text element.'
+        },
+        {
+            name: 'description',
+            type: 'string',
+            defaultValue: 'This is a text element.'
+        }
+    ]
+});
---
+++ sections/Text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Text = () => {
+    return (
+        <div>
+            Static Text...
+        </div>
+    )
+}
+
+export default Text
